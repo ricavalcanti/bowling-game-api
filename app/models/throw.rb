aef930ad7e5b7b839ced24e7aa36f692c3cc2287
Ricardo <ricavalcanti@bct.ect.ufrn.br>
2021-09-21 21:40:39 -0300

    fix: removing not implement method

--- app/models/throw.rb
+++ app/models/throw.rb
@@ -1,13 +1,3 @@
 class Throw < ApplicationRecord
   has_and_belongs_to_many :frames
-
-  before_destroy :remove_from_frames
-
-  private
-
-  def remove_from_frames
-    frames.each do |frame|
-      frame.remove_throw(self)
-    end
-  end
 end
