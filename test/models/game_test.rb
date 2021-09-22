0fe37cd3147b0d6192bd9a5efcbced854f697027
Ricardo <ricavalcanti@bct.ect.ufrn.br>
2021-09-22 02:26:57 -0300

    test: adding test to default values and fix some code style

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -3,9 +3,6 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   setup do
     @game = games(:first_game)
     @frames = frames(:first_frame, :second_frame)
@@ -13,39 +10,46 @@ class GameTest < ActiveSupport::TestCase
   end
 
   test 'update_game_with_throw should update an game and your respective frames' do
-    # given
-    expected_throws = count_game_throws(@game) + 1
-
-    # when
+    # given # when
     throw = Throw.new({ knocked_pins: 5 })
     @game.update_game_with_throw(throw)
 
     # then
+    expected_throws = count_game_throws(@game) + 1
     assert_equal(expected_throws, count_game_throws(@game))
   end
 
   test 'create_game_frames should create an empty frame when no params passed' do
     # given
     empty_game = games(:empty_game)
-    expected_frames_number = 1
 
     # when
     empty_game.create_game_frames(nil)
 
     # then
+    expected_frames_number = 1
     assert_equal(expected_frames_number, empty_game.frames.count)
   end
 
   test 'create_game_frames should create all given frames for a game' do
     # given
     empty_game = games(:empty_game)
-    expected_frames_number = @frames.count
 
     # when
     empty_game.create_game_frames(get_frames_as_hash(@frames))
 
     # then
+    expected_frames_number = @frames.count
     assert_equal(expected_frames_number, empty_game.frames.count)
+  end
+
+  test 'should use default values when save game without attr' do
+    # given # when
+    new_game = Game.create({})
+
+    # then
+    expected_game_attr = { 'total_score' => 0, 'status' => 'running'}
+    assert_equal(expected_game_attr, new_game.attributes.except('id', 'created_at', 'updated_at'))
   end
 
   private
